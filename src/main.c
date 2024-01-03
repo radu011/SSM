6cade6b6eb08a05a0eb6eb2b1490057cec9bc8f8
Toader Radu <toaderradu21@gmail.com>
2024-01-03 22:51:30 +0200

    Modifed main function
    Signed-off-by: Toader Radu <toaderradu21@gmail.com>

--- src/main.c
+++ src/main.c
@@ -9,15 +9,15 @@ int main(void)
 	UART0_Init(115200);
 	Buzzer_Init();
 	ADC0_Init();
-	
+
 	PIT_Init();
 	Init_SysTick();
 	RGBLed_Init();
-	
-	Light_Init(); 
+
+	Light_Init();
 
 	float prev = 0;
-	
+
 	for (;;)
 	{
 		if (play == 1)
@@ -25,28 +25,50 @@ int main(void)
 			Buzzer_Sing();
 			play = 0;
 		}
-				
-		if(flagADC)
+
+		if (flagADC)
 		{
 			float measured_voltage = 3.3f - (analog_input * 3.3f) / 1023;
-			
-			uint8_t parte_zecimala = (uint8_t) measured_voltage;
+
+			uint8_t parte_zecimala = (uint8_t)measured_voltage;
 			uint8_t parte_fractionara1 = ((uint8_t)(measured_voltage * 10)) % 10;
 			uint8_t parte_fractionara2 = ((uint8_t)(measured_voltage * 100)) % 10;
-			
+
 			uint8_t lightData = Light_Read() + 0x30;
-				
+
 			UART0_Transmit(parte_zecimala + 0x30);
 			UART0_Transmit(parte_fractionara1 + 0x30);
 			UART0_Transmit(parte_fractionara2 + 0x30);
 			UART0_Transmit(lightData);
-			
-			if(prev < 0.2 && measured_voltage > 0.4)
+
+			if (prev < 0.2 && measured_voltage > 0.4)
 				Buzzer_Sound(2);
-			else if(prev < 0.2 && measured_voltage > 0.2)
+			else if (prev < 0.2 && measured_voltage > 0.2)
 				Buzzer_Sound(1);
-			
-			flagADC=0;
-		} 
+
+			if (prev < 0.2)
+			{
+				if (measured_voltage > 0.4)
+					Buzzer_Sound(3);
+				if (measured_voltage > 0.2)
+					Buzzer_Sound(2);
+			}
+			else if (prev > 0.4)
+			{
+				if (measured_voltage < 0.2)
+					Buzzer_Sound(1);
+				if (measured_voltage < 0.4)
+					Buzzer_Sound(2);
+			}
+			else
+			{
+				if (measured_voltage > 0.4)
+					Buzzer_Sound(3);
+				if (measured_voltage < 0.2)
+					Buzzer_Sound(1);
+			}
+
+			flagADC = 0;
+		}
 	}
 }
